94b12e93b7ea5f6b267de0b3de2568342a0a5b43
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-10-31 07:57:30 +0200

    Roles

---
+++ TestApiJwt/Migrations/20221031053611_seedDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApiJwt.Migrations
+{
+    public partial class seedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            
+            migrationBuilder.InsertData(
+                
+                table: "AspNetRoles",
+                columns: new[] { "Id","Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] {Guid.NewGuid().ToString(),"user","user".ToUpper(),Guid.NewGuid().ToString()}
+             );
+            migrationBuilder.InsertData(
+
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), "admin", "admin".ToUpper(), Guid.NewGuid().ToString() }
+             );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM AspNetRoles");
+        }
+    }
+}
